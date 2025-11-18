364acad4a4fc3784f7118e156cfa1a1ad8670c8f
KimmyWingz <127132791+fishpeopleapps@users.noreply.github.com>
2025-11-18 11:23:47 -0700

    completed ext

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# SaneCrumbs
-This is a combination of SaneCase and JSBreadCrumbs Mediawiki Extensions with alterations. 
+# SaneCase
+
+## Description
+This extension is a mirror of the SaneCase extension publically available from MediaWiki, written by Martin Tournoij. 
+https://www.mediawiki.org/wiki/Extension:SaneCase
+This mirror contains the necessary composer file needed to include it in the Space-Wiki. 
+
+## Installation
+1. Add via composer locally - updating your composer.json and composer.lock file
+2. Add extension via a COPY statement in the Dockerfile
+3. Enable extension in the LocalSettings.php file
+
+## Usage
+The SaneCase extension automatically corrects case mistakes. For example if the page Test exists, and someone goes to TEST, they will be automatically redirected to Test with a 301.
+
+## Support/Roadmap
+This extension is pretty cut and dry and will probably be used to house other misc. hooks and php files we want to implement into the wiki. 
+
+## Project status
+n/a
+
---
+++ resources/JSBreadCrumbs.js
@@ -0,0 +1,181 @@
+var jsbreadcrumbs_controller = ( function () {
+	'use strict';
+
+	return {
+
+		initialize: function () {
+
+			var config = mw.config.get( 'JSBreadCrumbs' ),
+				pageName = config.PageName,
+				action = config.Action,
+				maxLength = config.MaxLength,
+				siteMaxCrumbs = config.SiteMaxCrumbs,
+				globalMaxCrumbs = config.GlobalMaxCrumbs,
+				domain = config.Domain,
+				showAction = config.ShowAction,
+				showSite = config.ShowSite,
+				horizontal = config.Horizontal,
+				horizontalSeparator = config.HorizontalSeparator,
+				cssSelector = config.CSSSelector,
+				leadingDescription = config.LeadingDescription,
+
+				siteName = mw.config.get( 'wgSiteName' ),
+
+				// get the breadcrumbs from the cookie
+				breadcrumbs = mw.cookie.get( 'mwext-jsbreadcrumbs' );
+			if ( breadcrumbs ) {
+				try {
+					breadcrumbs = JSON.parse( breadcrumbs );
+				} catch ( e ) {
+					breadcrumbs = [];
+				}
+			} else {
+				breadcrumbs = [];
+			}
+
+			// remove this URL from the breadcrumb list if it is already in it
+			var url = location.pathname + location.search,
+				index = 0;
+			while ( index < breadcrumbs.length ) {
+				if ( breadcrumbs[ index ].url === url ) {
+					breadcrumbs.splice( index, 1 );
+				} else {
+					index++;
+				}
+			}
+
+			// add the current URL to the breadcrumb list if it points
+			// to a valid page
+			if ( pageName.slice( Math.max( 0, pageName.length - 8 ) ) !== 'Badtitle' ) {
+				breadcrumbs.push({
+					url: url,
+					title: sanitize(pageName),
+					action: sanitize(action),
+					siteName: sanitize(siteName)
+				});
+			}
+
+			// get the list of breadcrumbs to display
+			var visibleCrumbs = [];
+			for ( index = breadcrumbs.length - 1; index >= 0; index-- ) {
+				if ( domain || breadcrumbs[ index ].siteName === siteName ) {
+					if ( visibleCrumbs.length < siteMaxCrumbs ) {
+						var breadcrumb = breadcrumbs[ index ];
+						if ( !( 'action' in breadcrumb ) || showAction ||
+							breadcrumb.action.length === 0 ) {
+								var a = document.createElement( 'a' );
+								a.href = breadcrumb.url;
+
+								var linkText = '';
+								if ( showSite ) {
+									linkText += breadcrumb.siteName + ': ';
+								}
+
+								var title = breadcrumb.title;
+								if ( title.length > maxLength ) {
+									title = title.slice( 0, Math.max( 0, maxLength ) ) + '...';
+								}
+								if ( 'action' in breadcrumb && breadcrumb.action.length > 0 ) {
+									title += ' [' + breadcrumb.action + ']';
+								}
+								linkText += title;
+
+								a.textContent = linkText;
+								visibleCrumbs.push( a.outerHTML );
+						} else {
+							breadcrumbs.splice( index, 1 );
+						}
+					} else {
+						breadcrumbs.splice( index, 1 );
+					}
+				}
+			}
+
+			// truncate the breadcrumb list if necessary
+			if ( breadcrumbs.length > globalMaxCrumbs ) {
+				breadcrumbs = breadcrumbs.slice( breadcrumbs.length - globalMaxCrumbs );
+			}
+
+			// save the breadcrumbs to the cookie, sanitize first
+			function sanitize(str) {
+				return typeof str === 'string'
+					? str.replace(/<[^>]*>/g, '') // removes full HTML tags
+					: str;
+			}
+
+			const safeBreadcrumbs = breadcrumbs.map(b => ({
+				url: sanitize(b.url),
+				title: sanitize(b.title),
+				action: sanitize(b.action),
+				siteName: sanitize(b.siteName)
+			}));
+
+			// For Debugging
+			// console.log('Try 1: Sanitized breadcrumbs:', safeBreadcrumbs);
+
+			mw.cookie.set(
+				'mwext-jsbreadcrumbs',
+				JSON.stringify( safeBreadcrumbs ),
+				{ path: '/', expires: 30, secure: window.location.protocol === 'https:', sameSite: 'Strict' }
+			);
+
+
+			var skin = mw.config.get( 'skin' ),
+
+				selector;
+			if ( horizontal ) {
+				if ( skin in cssSelector ) {
+					selector = cssSelector[ skin ];
+				} else if ( skin === 'vector-2022' ) {
+					selector = '#mw-content-subtitle';
+				} else if ( skin === 'foreground' ) {
+					selector = '#mw-js-message';
+				} else {
+					selector = '#top';
+				}
+
+				$( selector ).before(
+					'<span id="mwext-bc" class="noprint plainlinks jsbc-breadcrumbs"></span>' );
+				var mwextbc = $( '#mwext-bc' );
+				mwextbc.append( leadingDescription + ' ' );
+				for ( index = visibleCrumbs.length - 1; index >= 0; index-- ) {
+					mwextbc.append( visibleCrumbs[ index ] );
+					if ( index > 0 ) {
+						mwextbc.append( ' ' + horizontalSeparator + ' ' );
+					}
+				}
+			} else {
+				if ( skin in cssSelector ) {
+					selector = cssSelector[ skin ];
+				} else if ( skin === 'vector' || skin === 'vector-2022' ) {
+					selector = '#p-tb';
+				} else {
+					selector = '#column-one';
+				}
+
+				if ( skin === 'vector' || skin === 'vector-2022' ) {
+					$( selector ).after( "<div class='portal' id='p-rv' role='navigation' aria-labelledby='p-rv-label'></div>" );
+					$( '#p-rv' ).append( $( '<h3>' ).attr( 'id', 'p-rv-label' ).text( leadingDescription ) );
+					$( '#p-rv' ).append( "<div class='body' style='display: block;'><ul id='p-rv-list2'></ul>" );
+				} else {
+					$( selector ).append( "<div class='portlet' id='p-rv' role='navigation'></div>" );
+					$( '#p-rv' ).append( '<h3>' + leadingDescription + "</h3><div class='pBody'><ul id='p-rv-list2'></ul></div>" );
+				}
+
+				for ( index = 0; index < visibleCrumbs.length; index++ ) {
+					$( '#p-rv-list2' ).append( '<li>' + visibleCrumbs[ index ] + '</li>' );
+				}
+			}
+		}
+	};
+}() );
+
+window.JSBreadCrumbsController = jsbreadcrumbs_controller;
+
+( function () {
+	$( function () {
+		if ( mw.config.exists( 'JSBreadCrumbs' ) ) {
+			window.JSBreadCrumbsController.initialize();
+		}
+	} );
+}() );
